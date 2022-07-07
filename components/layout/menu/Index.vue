c840d2c1aa069556eba067af4e7e3c4050097b66
NMTuan <nmtuan@qq.com>
2022-07-07 11:30:11 +0800

    优化菜单渲染. 保证及时更新

--- components/layout/menu/Index.vue
+++ components/layout/menu/Index.vue
@@ -2,7 +2,7 @@
  * @Author: NMTuan
  * @Email: NMTuan@qq.com
  * @Date: 2022-07-05 13:46:55
- * @LastEditTime: 2022-07-06 14:42:17
+ * @LastEditTime: 2022-07-07 11:28:47
  * @LastEditors: NMTuan
  * @Description: 
  * @FilePath: \ezAdmin3\components\layout\menu\Index.vue
@@ -14,7 +14,17 @@
 </template>
 <script setup>
 const auth = useAuth()
-const firsrLevelPages = auth.authorizedPages.filter((page) => {
+const firsrLevelPages = ref([])
+
+firsrLevelPages.value = auth.authorizedPages.filter((page) => {
     return page.level === 1 && page.dynamicRoute === false
+})
+
+// 保证权限变换后, 能及时渲染菜单
+watch(() => auth.authorizedPages, (newVal) => {
+    console.log('newVal', newVal)
+    firsrLevelPages.value = newVal.filter((page) => {
+        return page.level === 1 && page.dynamicRoute === false
+    })
 })
 </script>
