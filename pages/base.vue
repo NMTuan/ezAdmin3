683e70da0e442da63d0edb0a82668ca7e0a148ff
NMTuan <nmtuan@qq.com>
2022-07-13 11:33:45 +0800

    perf: 调整目录结构

---
+++ pages/base.vue
@@ -0,0 +1,19 @@
+<!--
+ * @Author: NMTuan
+ * @Email: NMTuan@qq.com
+ * @Date: 2022-07-13 10:16:26
+ * @LastEditTime: 2022-07-13 10:27:53
+ * @LastEditors: NMTuan
+ * @Description: 
+ * @FilePath: \ezAdmin3\pages\my.vue
+-->
+<template>
+    <NuxtPage />
+</template>
+<script>
+export default {
+    page: {
+        name: 'Components'
+    }
+}
+</script>
